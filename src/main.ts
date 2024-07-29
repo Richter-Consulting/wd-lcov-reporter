523632a714e52bcaeb55d22eb1d20864d8364083
Eugen [WebDucer] Richter <github@richter.consulting>
2024-07-29 11:09:52 +0200

    Add first implementation for PR comment

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import fs from 'fs'
 import { LcovParserConfigType } from './types'
 import { parseLcov } from './parse-lcov'
@@ -14,9 +15,9 @@ export async function run(): Promise<void> {
     const lcovFileName = core.getInput('lcov-file')
     const excludeFiles = core.getInput('exclude-files')
     const stepSummaryInput = core.getInput('step-summary')
+    const prCommentInput = core.getInput('pr-comment')
 
     // Check, the file name is set and exists
-    core.debug(`stepSummaryInput: ${stepSummaryInput}`)
     if (!lcovFileName) {
       core.setFailed('File name is required')
       return
@@ -43,9 +44,23 @@ export async function run(): Promise<void> {
 
     // Set step summary, if requested
     if (stepSummaryInput === 'true') {
-      const summary = `# Coverage Summary
+      const summary = _generateSummary(overallCoverage, markdownTable)
+      core.summary.addRaw(summary).write()
+    }
 
-Overall coverage: <strong>${overallCoverage}</strong>
+    // Set PR comment, if requested
+    if (prCommentInput === 'true') {
+      _handlePrComment(overallCoverage, markdownTable)
+    }
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+function _generateSummary(overallCoverage: string, markdownTable: string) {
+  return `# Coverage Summary
+
+Overall coverage: **${overallCoverage} %**
 
 <details><summary>Detailed coverage</summary>
 
@@ -53,10 +68,50 @@ ${markdownTable}
 
 </details>
 `
-      core.summary.addRaw(summary).write()
+}
+
+async function _handlePrComment(
+  overallCoverage: string,
+  markdownTable: string
+) {
+  const prNumber = github.context.payload.pull_request?.number
+  if (!prNumber || prNumber <= 0) {
+    core.warning('PR number not found. Skipping PR comment.')
+    return
+  }
+
+  const commentTag = `<!-- Richter-Constulting/wd-lcov-reporter-action for PR: ${prNumber} -->`
+
+  // Search for the comment with the given tag
+  const octokit = github.getOctokit(core.getInput('github-token'))
+  let commentId = -1
+  for await (const { data: comments } of octokit.paginate.iterator(
+    octokit.rest.issues.listComments,
+    { ...github.context.repo, issue_number: prNumber }
+  )) {
+    const foundComment = comments.find(comment =>
+      comment.body?.includes(commentTag)
+    )
+    if (foundComment) {
+      commentId = foundComment.id
+      break
     }
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+  }
+
+  const summary = _generateSummary(overallCoverage, markdownTable)
+  // If comment, found, replace the existing comment
+  if (commentId > 0) {
+    await octokit.rest.issues.updateComment({
+      ...github.context.repo,
+      comment_id: commentId,
+      body: summary
+    })
+  } else {
+    // If not found, create a new comment
+    await octokit.rest.issues.createComment({
+      ...github.context.repo,
+      issue_number: prNumber,
+      body: summary
+    })
   }
 }
